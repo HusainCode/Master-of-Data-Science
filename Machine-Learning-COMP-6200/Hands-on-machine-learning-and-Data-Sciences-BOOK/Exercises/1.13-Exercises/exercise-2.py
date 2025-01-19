0d937e1a3f3c53bf2a298e20e1b60d1a74d6f278
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-01-19 07:53:06 -0600

    Update exercise-2.py

--- Machine-Learning-COMP-6200/Hands-on-machine-learning-and-Data-Sciences-BOOK/Exercises/1.13-Exercises/exercise-2.py
+++ Machine-Learning-COMP-6200/Hands-on-machine-learning-and-Data-Sciences-BOOK/Exercises/1.13-Exercises/exercise-2.py
@@ -1,1 +1,12 @@
+# Write a Python program that takes two numbers from the user and finds their power, that is, ( x^y ).
 
+
+def convert_to_power(num1, exponent):
+  power = pow(num1,num2)
+  return power
+
+
+num = int(input("Enter a number:")
+exponent  = int(input("Enter a exponent of this number:")
+
+print( "The power of , num ,convert_to_power(num, power_of_num)
