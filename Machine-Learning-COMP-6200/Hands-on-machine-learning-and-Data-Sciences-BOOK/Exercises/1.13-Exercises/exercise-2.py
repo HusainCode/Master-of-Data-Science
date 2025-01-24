b7ae1065815031bca3237b01d3b75dbdd500dea8
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-01-24 04:08:03 -0600

    Update exercise-2.py

--- Machine-Learning-COMP-6200/Hands-on-machine-learning-and-Data-Sciences-BOOK/Exercises/1.13-Exercises/exercise-2.py
+++ Machine-Learning-COMP-6200/Hands-on-machine-learning-and-Data-Sciences-BOOK/Exercises/1.13-Exercises/exercise-2.py
@@ -1,12 +1,28 @@
 # Write a Python program that takes two numbers from the user and finds their power, that is, ( x^y ).
 
-
-def convert_to_power(num1, exponent):
-  power = pow(num1,num2)
+# This function does the math
+def calculate_power(base, exponent):
+  power = pow(round (base,exponent),2) 
   return power
 
+# Map numbers to their Unicode characters
+def convert_to_superscript(base, exponent):
 
-num = int(input("Enter a number:")
-exponent  = int(input("Enter a exponent of this number:")
+    superscripts = {
+        "0": "\u2070", "1": "\u00B9", "2": "\u00B2", "3": "\u00B3",
+        "4": "\u2074", "5": "\u2075", "6": "\u2076", "7": "\u2077",
+        "8": "\u2078", "9": "\u2079"
+    }
+    #
+    exponent_str = ''.join(superscripts[digit] for digit in str(exponent))
+    return str(base) + exponent_str
 
-print( "The power of , num ,convert_to_power(num, power_of_num)
+base = int(input("Enter a base number:"))
+exponent  = int(input("Enter an exponent:"))
+
+print( "The power of", convert_to_superscript(base, exponent), "is" , calculate_power(base, exponent))
+
+# Sample of the output: 
+Enter a base number:2
+Enter an exponent:2
+The power of 2² is 4
