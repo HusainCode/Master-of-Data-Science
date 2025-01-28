de014f2334d2388b30a4de6f31c6df3d8e40cadf
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-01-28 04:38:51 -0600

    Create Assignment1-Python-Review.py

---
+++ Data-Visualization-5850/Assignment1-Python-Review.py
@@ -0,0 +1,75 @@
+# Student Name: Husain Alshaikhahmed
+# Student ID: T004560868
+# Class: COMP 5850
+# Instructor: Professor Dr. Caleb Vatral
+# Assignment: Python Review
+# Due Date: January 22, 2025, 11:59 PM
+
+# Description :
+
+
+
+# Task:
+# Part 3: Regression
+# Using Scikit-learn, create a linear regression model to predict a fish’s weight based on the other numerical features (length1, length2, length3, height, and width). Ensure the model is trained only on the training split.
+# Print the coefficients and intercept of the trained regression model.
+# Calculate and print the model’s accuracy (e.g., R^2 score) on both the training and testing splits.
+
+# Part 4: Classification
+# Using Scikit-learn, create a fully-grown decision tree classifier to predict a fish’s species using its length, width, and height measurements. Ensure that the model does not use the fish’s weight in the calculations, and that it is trained only on the training split.
+# Calculate and print the model’s accuracy on both the training and testing splits. Additionally, print the depth of the fully grown decision tree.
+# Optimize the decision tree by determining the best maximum depth:
+# Train multiple decision tree models with the max_depth parameter ranging from 1 to the depth of the fully-grown tree.
+# For each depth, record the training and testing accuracy.
+# Use this information to identify the depth that provides the best generalization performance.
+
+
+
+# Part 1: Data Preparation
+# Using Pandas, download and load the fish dataset from the provided URL.
+# Using Scikit-learn’s train_test_split function, create an 80/20 train/test split of the data, ensuring that the split is stratified by species (to maintain representation in both splits).
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Data path
+FILE_PATH = '/root/development_tools/fish (1).csv'
+# Loading the csv data with pandas
+data = pd.read_csv(FILE_PATH)
+
+# We drop "Species" to predict the species
+x_features = data.drop(columns=['Species']) # x is the input
+
+# predcting the fish species is a classification task, hence:
+y_target = data["Species"] # target value: Bream, Pike, Perch, 
+                                   #               Roach, Whitefish, Parkki,
+                                   #               Perch, Pike, Smelt 
+
+
+# Preparing and training the 
+x_train, y_train, x_test, y_test = train_test_split(x_features,y_target, test_size=0.2, stratify=y_target)
+
+
+
+
+# Part 2: Statistics
+# Slice the full dataset by fish species
+# For each species, calculate and print the mean, standard deviation, and interquartile range (IQR) for each numerical feature
+# (e.g., length1, length2, length3, height, width, and weight). Use Pandas and NumPy for your calculations. 
+# You can calculate the IQR using the difference between the 75th and 25th percentiles.
+
+# slice the data using groupby() from pandas 
+sliced_species = data.groupby('Species')
+
+bream = sliced_species.get_group('Bream')
+# pike = sliced_species.get_group("Pike")
+# perch = sliced_species.get_group("Perch")
+# roach = sliced_species.get_group("Roach")
+# whitefish = sliced_species.get_group("Whitefish")
+# parkki = sliced_species.get_group("Parkki")
+# Perch = sliced_species.get_group("Perch")
+# Pike = sliced_species.get_group("Pike")
+# smelt = sliced_species.get_group("Smelt")
+
+bream_mean = bream.mean()
+print(bream_mean)
